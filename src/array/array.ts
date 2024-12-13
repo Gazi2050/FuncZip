a4942d3c05fcfc1a8667c546fa6759e6f741ab8c
Gazi2050 <gazi45344@gmail.com>
2024-12-13 22:25:03 +0600

    feat:chunkArray function added

--- src/array/array.ts
+++ src/array/array.ts
@@ -1,1 +1,10 @@
-console.log('array');
+export function chunkArray<T>(arr: T[], size: number): T[][] {
+    const result: T[][] = [];
+
+    for (let i = 0; i < arr.length; i += size) {
+        const chunk = arr.slice(i, i + size);
+        result.push(chunk);
+    }
+
+    return result;
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,4 @@
-//for test
+import { chunkArray } from "./src/array/array"
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const testChunkArray = chunkArray(arr, 3)
+console.log(testChunkArray);
